2f2bc25acbab81b07b5cd773f930ff3d0e6b7752
Ian Davies <ian.davies@timgroup.com>
2012-10-09 00:21:15 +0200

    Add first failing test (copied from local project)

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clj-iso8583 "0.1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
