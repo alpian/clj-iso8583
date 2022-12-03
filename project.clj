964e5d8efd8f99bae51c66e216a275ad147c9689
Chris Oakman <chris@oakmac.com>
2022-12-02 23:11:53 -0600

    add description and url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject com.oakmac.iso8583 "0.2.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Parse ISO 8583 messages into Clojure maps and vice versa (updated fork of alpian/clj-iso8583)"
+  :url "https://github.com/oakmac/com.oakmac.iso8583"
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :source-paths ["src-clj/"]
   :java-source-paths ["src-java/"]
