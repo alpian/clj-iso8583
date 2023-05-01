14ccd4dbc17c89c148453c8e7a560a5a3e9b91cc
Chris Oakman <chris@oakmac.com>
2023-05-01 10:56:00 -0500

    WIP WIP WIP

--- test/com/oakmac/iso8583/util/string_test.clj
+++ test/com/oakmac/iso8583/util/string_test.clj
@@ -6,6 +6,7 @@
 (deftest left-pad-test
   (is (= (util.str/left-pad "" 0 "") ""))
   (is (= (util.str/left-pad " " 2 "1") "1 "))
+  (is (= (util.str/left-pad "17" 3 "0") "017"))
   (is (= (util.str/left-pad "foo" 2 "*") "foo"))
   (is (= (util.str/left-pad "foo" 3 "*") "foo"))
   (is (= (util.str/left-pad "foo" 4 "*") "*foo"))
--- test/com/oakmac/iso8583/writer_test.clj
+++ test/com/oakmac/iso8583/writer_test.clj
@@ -18,52 +18,12 @@
     (is (= (binary/binary-string-to-hex (writer/write-bitmap [129]))
            (str "8000000000000000" "8000000000000000" "8000000000000000")))))
 
-
-
 (deftest write-fields-test
   (testing "Can write a few fields"
     (is (= (binary/bytes-to-hex
-            (writer/write (format-iso8583/field-definitions)
-                          {:message-type "0200"
-                           :pan "1111222233334444"
-                           :processing-code "011000"
-                           :transaction-amount "000000006660"
-                           ;; This is a 3 digit variable length field with
-                           ;; length 17. Expect it to be 0 padded to 017.
-                           :message-reason-code "because i said so"}))
-           (str (binary/bytes-to-hex "0200") "7000000000000000" (binary/bytes-to-hex "161111222233334444011000000000006660017because i said so"))))))
-
-
-
-; (deftest write-fields-test
-;   (testing "Can write a few fields"
-;     (is (= (binary/bytes-to-hex
-;              (writer/write (format-iso8583/field-definitions)
-;                {:message-type "0200"
-;                 :pan "1111222233334444"
-;                 :processing-code "011000"
-;                 :transaction-amount "000000006660"
-;                 :message-reason-code "mary had a little lamb"}))
-;            (str (binary/bytes-to-hex "0200")
-;                 "7000000000000000"
-;                 (binary/bytes-to-hex "161111222233334444011000000000006660017mary had a little lamb"))))))
-
-
-
-    ; (is (= (binary/bytes-to-hex
-    ;         (writer/write (format-iso8583/field-definitions)
-    ;                       {:message-type "0200"
-    ;                        :pan "1111222233334444"
-    ;                        :processing-code "011000"
-    ;                        :transaction-amount "000000006660"}))
-    ;                        ;; This is a 3 digit variable length field with
-    ;                        ;; length 17. Expect it to be 0 padded to 017.
-
-    ;        (str (binary/bytes-to-hex "0200")
-    ;             "7000000000000000"
-    ;             (binary/bytes-to-hex "161111222233334444011000000000006660017mary had a little lamb"))
-    ;        "variable length fields get zero-padded"))))
-
-
-; (not (= "3032303070000000000001003136303131313132323232333333333434343430313130303030303030303030303636363032323232306D617279206861642061206C6974746C65206C616D62"
-;         "3032303070000000000000003136313131313232323233333333343434343031313030303030303030303030363636306D617279206861642061206C6974746C65206C616D62"))
+             (writer/write (format-iso8583/field-definitions)
+               {:message-type "0200"
+                :pan "1111222233334444"
+                :processing-code "011000"
+                :transaction-amount "000000006660"}))
+           (str (binary/bytes-to-hex "0200") "7000000000000000" (binary/bytes-to-hex "161111222233334444011000000000006660"))))))
